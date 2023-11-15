0d10a6e6f173c6ed6ab91c7f907311aaa444191c
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-11-15 14:45:37 +0530

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter} from "react-router-dom";
+import { useSelector } from "react-redux";
+
+// import LoginPage from "./pages/LoginPage";
+// import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import Header from "./components/layout/header/Header";
+import Footer from "./components/layout/footer/footer";
+import RegisterLogin from "./components/RegisterLogin";
+import SideBAr from "./components/layout/header/SideBAr";
+import CreateQuizPage from "./pages/CreateQuizPage";
+import ReportPage from "./pages/ReportPage";
+import NewQuizPage from "./pages/NewQuizPage";
 
 function App() {
+  const [isSideBarVisibe,setIsSideBarVisibe] = useState(false)
+  const user = useSelector((state) => state.token);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!user.token ? (
+        <RegisterLogin />
+      ) : (
+        <BrowserRouter>
+          <Header setIsSideBarVisibe={setIsSideBarVisibe} isSideBarVisibe={isSideBarVisibe} />
+          <main className="font-[karla] relative px-6 ">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/create-quiz-home" element={<CreateQuizPage />} />
+              <Route path="/report" element={<ReportPage />} />
+              <Route path="/create-new-quiz" element={<NewQuizPage />} />
+            </Routes>
+            {isSideBarVisibe?<SideBAr userName={user.name} setIsSideBarVisibe={setIsSideBarVisibe} />:""}
+          </main>
+          <Footer />
+        </BrowserRouter>
+      )}
+    </>
   );
 }
 
